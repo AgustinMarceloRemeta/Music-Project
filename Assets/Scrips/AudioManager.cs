313aaee650a1f8be527ff28d57f06b939e8f1e50
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-11-02 10:22:50 -0300

    Save Music

--- Assets/Scrips/AudioManager.cs
+++ Assets/Scrips/AudioManager.cs
@@ -1,22 +1,56 @@
 using UnityEngine;
-using UnityEngine.Audio;
+using System.Collections.Generic;
+using System.Collections;
 
 public class AudioManager : MonoBehaviour
 {
-    AudioSource audioSource;
+    [SerializeField]AudioSource audioSource;
+    [SerializeField] float timeSave;
+    [SerializeField] List<Subtitle> subtitles; 
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         SetVolume();
         SetMute();
+        audioSource.time = PlayerPrefs.GetFloat("timeSound", 0);
+    }
+    private void Start()
+    {
+        InvokeRepeating("SaveTime", timeSave, timeSave);
+    }
+    private void Update()
+    {
+        
     }
 
     public void SetVolume()
     {
+        if(audioSource != null)
         audioSource.volume = PlayerPrefs.GetFloat("volume", 1);
     }
     public void SetMute()
     {
-        audioSource.mute = (PlayerPrefs.GetInt("mute", 0) == 1 ? true : false);
+        if (audioSource != null)
+            audioSource.mute = (PlayerPrefs.GetInt("mute", 0) == 1 ? true : false);
     } 
+
+    public void SaveTime()
+    {
+        PlayerPrefs.SetFloat("timeSound", audioSource.time); 
+    }
+
+    IEnumerator Subtitles()
+    {
+        foreach (var subtitle in subtitles)
+        {
+            
+        }
+        yield return new WaitForSeconds(2);
+    }
+}
+public class Subtitle : MonoBehaviour
+{
+    float timeToInit;
+    string subtitleText;
+    Subtitle nextSubtitle;
 }
