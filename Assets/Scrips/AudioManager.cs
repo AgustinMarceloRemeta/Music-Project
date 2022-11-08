15527c2b23c0d3db9c85da3ed7e18ea0a70327ff
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-11-08 16:24:17 -0300

    Patch /refactor/stetic /error correction
    Patch: part b
    refactor: general
    stetic: Menu
    error: Play button

--- Assets/Scrips/AudioManager.cs
+++ Assets/Scrips/AudioManager.cs
@@ -6,83 +6,86 @@ using System.Linq;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] float timeSave;
-    [SerializeField] Text textSubtitle;
-    [SerializeField] Scrollbar realTimeMusic;
-    [SerializeField] List<Subtitle> subtitles;
-    [SerializeField] List<float> timeSubtitles;
-    AudioSource audioSource;
-    Subtitle actualSubtitle;
+    [SerializeField] float _timeSave;
+    [SerializeField] Text _textSubtitle;
+    [SerializeField] Scrollbar _realTimeMusic;
+    [SerializeField] List<Subtitle> _subtitles;
+    [SerializeField] List<float> _timeSubtitles;
+    AudioSource _audioSource;
+    Subtitle _actualSubtitle;
 
     void Awake()
     {
-        foreach (Subtitle item in FindObjectsOfType<Subtitle>()) subtitles.Add(item);
-        audioSource = GetComponent<AudioSource>();
+        foreach (Subtitle item in FindObjectsOfType<Subtitle>()) _subtitles.Add(item);
+        _audioSource = GetComponent<AudioSource>();
         SetVolume();
         SetMute();
-        audioSource.time = PlayerPrefs.GetFloat("timeSound", 0);
+        _audioSource.time = PlayerPrefs.GetFloat("timeSound", 0);
     }
 
     private void Start()
     {
-        InvokeRepeating("SaveTime", timeSave, timeSave);
+        InvokeRepeating("SaveTime", _timeSave, _timeSave);
         StartCoroutine(Subtitles(PlayerPrefs.GetFloat("timeSound", 0)));
     }
 
-    private void Update() => realTimeMusic.size = audioSource.time / audioSource.clip.length;
+    private void Update() => _realTimeMusic.size = _audioSource.time / _audioSource.clip.length;
 
     public void SetVolume()
     {
-        if(audioSource != null) audioSource.volume = PlayerPrefs.GetFloat("volume", 1);
+        if(_audioSource != null) _audioSource.volume = PlayerPrefs.GetFloat("volume", 1);
     }
 
     public void SetMute()
     {
-        if (audioSource != null) audioSource.mute = (PlayerPrefs.GetInt("mute", 0) == 1 ? true : false);
+        if (_audioSource != null) _audioSource.mute = (PlayerPrefs.GetInt("mute", 0) == 1 ? true : false);
     } 
 
-    public void SaveTime() => PlayerPrefs.SetFloat("timeSound", audioSource.time); 
+    public void SaveTime() => PlayerPrefs.SetFloat("timeSound", _audioSource.time); 
 
      IEnumerator Subtitles(float time)
     {
         Subtitle newSubtitle = GetNewSubtitle(time);
-        textSubtitle.text = newSubtitle.subtitleText;
-        actualSubtitle = newSubtitle;
+        _textSubtitle.text = newSubtitle.subtitleText;
+        _actualSubtitle = newSubtitle;
 
         if (newSubtitle.end)
         {
-            yield return new WaitForSeconds(audioSource.clip.length - time);
+            yield return new WaitForSeconds(_audioSource.clip.length - time);
             StartCoroutine(Subtitles(0));
         }
         else
         {
             yield return new WaitForSeconds(newSubtitle.nextSubtitle.timeToInit - time);
-            StartCoroutine(Subtitles(audioSource.time));
+            StartCoroutine(Subtitles(_audioSource.time));
         }
     }
 
     private Subtitle GetNewSubtitle(float audioTime)
     {
-        if (actualSubtitle != null)  return actualSubtitle.nextSubtitle;       
+        if (_actualSubtitle != null)  return _actualSubtitle.nextSubtitle;       
         else
         {
-            if (timeSubtitles != null) timeSubtitles.Clear();
+            if (_timeSubtitles != null) _timeSubtitles.Clear();
 
-            foreach (Subtitle item in subtitles) if (item.timeToInit <= audioTime) timeSubtitles.Add(item.timeToInit);
-            foreach (Subtitle item in subtitles) if (item.timeToInit == timeSubtitles.Max()) return item;
+            foreach (Subtitle item in _subtitles) if (item.timeToInit <= audioTime) _timeSubtitles.Add(item.timeToInit);
+            foreach (Subtitle item in _subtitles) if (item.timeToInit == _timeSubtitles.Max()) return item;
                
             return null;
         }
     }
 
-    public void ResetMusic() => audioSource.time = 0;
+    public void ResetMusic() => _audioSource.time = 0;
 
     public void InitSubtitles()
     {
-        actualSubtitle = null;
-        StartCoroutine(Subtitles(audioSource.time));
+        _actualSubtitle = null;
+        StartCoroutine(Subtitles(_audioSource.time));
     }
 
-
-}
+    public void ButtonPlay()
+    {
+        if (!_audioSource.isPlaying) _audioSource.Play();
+    }
+} 
 
--- Assets/Scrips/Menu.cs
+++ Assets/Scrips/Menu.cs
@@ -4,15 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] Slider sliderVolume;
-    [SerializeField] Toggle toggleMute;
-    [SerializeField] float maxValueVolume;
+    [SerializeField] Slider _sliderVolume;
+    [SerializeField] Toggle _toggleMute;
+    [SerializeField] float _maxValueVolume;
     private void Awake() => GetValues();
     
     private void GetValues()
     {
-        sliderVolume.value = PlayerPrefs.GetFloat("volume", maxValueVolume);
-        toggleMute.isOn = (PlayerPrefs.GetInt("mute", 0) == 1 ? true : false);
+        _sliderVolume.value = PlayerPrefs.GetFloat("volume", _maxValueVolume);
+        _toggleMute.isOn = (PlayerPrefs.GetInt("mute", 0) == 1 ? true : false);
     }
 
     public void ChangeLevel()
@@ -21,8 +21,16 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene(sceneActive>= SceneManager.sceneCount? sceneActive - 1 : sceneActive + 1);
     }
 
-    public void SaveVolume() => PlayerPrefs.SetFloat("volume", sliderVolume.value);
+    public void SaveVolume() => PlayerPrefs.SetFloat("volume", _sliderVolume.value);
     
-    public void SaveToggle() => PlayerPrefs.SetInt("mute", (toggleMute.isOn ? 1 : 0));
-    
+    public void SaveToggle() => PlayerPrefs.SetInt("mute", (_toggleMute.isOn ? 1 : 0));
+
+    public void Reset() => PlayerPrefs.SetFloat("timeSound", 0);
+
+    public void Default()
+    {
+        _toggleMute.isOn = false;
+        _sliderVolume.value = _maxValueVolume;
+    }
+
 }
